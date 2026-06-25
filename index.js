93fb77f25dbfb6dc071484d3bac059ea9929b512
Jaouad Sarouti <jaouadsarti@gmail.com>
2026-06-25 13:51:19 -0400

    simple form validation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const container = document.querySelector(".container");
+const city = document.getElementById("city");
 btn.addEventListener("click", function (e) {
+  const divBlock = document.createElement("div");
+  const paragraph = document.createElement("p");
+  const lat = document.createElement("p");
+  const long = document.createElement("p");
   e.preventDefault();
   let cityInput = encodeURIComponent(city.value);
+  validateInput(cityInput);
 
   const apiPoint = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityInput}?unitGroup=us&key=QU5HL3GNEYWVZ4U4N7AZDRS6C&contentType=json`;
   fetch(apiPoint)
@@ -17,11 +24,7 @@ btn.addEventListener("click", function (e) {
         latitude,
         longitude,
       } = { ...response };
-      const container = document.querySelector(".container");
-      const divBlock = document.createElement("div");
-      const paragraph = document.createElement("p");
-      const lat = document.createElement("p");
-      const long = document.createElement("p");
+
       paragraph.textContent = address;
       lat.textContent = latitude;
       long.textContent = longitude;
@@ -40,9 +43,15 @@ btn.addEventListener("click", function (e) {
         ulList.appendChild(liKey);
         ulList.classList.add("ulClass");
         divBlock.append(ulList);
-        console.log(val, "====", key);
       }
     })
     .catch((err) => console.log(err));
   city.value = "";
 });
+
+function validateInput(element) {
+  if (!element || element == null) {
+    container.innerHTML = `<div class="">  please verify the city name  </div>`;
+    return element;
+  }
+}
